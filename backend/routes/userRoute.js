9f8de8985162d2e7bf017ca8b2e8e7e7180931e2
matlavv <mathis.laversin92@gmail.com>
2023-12-14 11:24:54 +0100

    show timer by user and averageTimer method

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,27 +1,35 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
-const jwtMiddleware = require('../middlewares/jwtMiddleware'); // Assurez-vous que le chemin est correct
+const jwtMiddleware = require('../middlewares/jwtMiddleware');
 
-// Route pour l'inscription d'un utilisateur
-router.post('/register', userController.register);
-
-// Route pour obtenir tous les utilisateurs
-router.get('/', userController.getAllUsers);
-
-// Route pour la connexion d'un utilisateur
-router.post('/login', userController.login);
-
-// Route pour obtenir, modifier, supprimer un utilisateur spécifique
-// et pour obtenir les timers d'un utilisateur
+// Route pour l'inscription d'un user
 router
-    .route('/:userId')
-    .all(jwtMiddleware.verifyToken)
-    .get(userController.getUser)        // Obtenir un utilisateur spécifique
-    .put(userController.putUser)        // Modifier un utilisateur spécifique
-    .delete(userController.deleteUser); // Supprimer un utilisateur spécifique
+    .route('/register')
+    .post(userController.userRegister)
 
-// Route pour obtenir les timers d'un utilisateur spécifique
-router.get('/:userId/timers', jwtMiddleware.verifyToken, userController.getUserTimers);
+// Route pour la connection d'un user
+router
+    .route('/login')
+    .post(userController.userLogin)
+
+router
+    .route('/:user_id')
+    .all(jwtMiddleware.verifyToken) 
+    .get(userController.getUser) // Obtenir un user spécifique 
+    .put(userController.putUser) // Modifier un user spécifique 
+    .patch(userController.patchUser) // Modifier un user spécifique 
+    .delete(userController.deleteUser) // Supprimer un user spécifique 
+
+// Route pour obtenir les timers d'un user spécifique
+router
+    .route('/:user_id/timer')
+    .get(userController.getUserTimer)
+    .post(jwtMiddleware.verifyToken, userController.getUser)
+
+// Route pour obtenir la moyenne des timers d'un user spécifique
+router
+    .route('/:user_id/averageTimer')
+    .get(userController.getAverageTimerUser)
 
 module.exports = router;
